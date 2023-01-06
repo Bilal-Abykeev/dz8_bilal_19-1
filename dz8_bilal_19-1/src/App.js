8bd538eb38296ade5d5d54784a4890125eee235f
Bilal <113285404+Bilal-Abykeev@users.noreply.github.com>
2023-01-06 17:24:16 +0300

    Add files via upload

---
+++ dz8_bilal_19-1/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Contacts from './Contact';
+
+function App() {
+  return (
+    <div className="App">
+       <Contacts name="Temirlan"  >
+        <h3>Age:18</h3>
+        <div>НЕ ЖЕНАТ</div>
+       </Contacts>
+    </div>
+  );
+}
+<Contacts/>
+export default App;
